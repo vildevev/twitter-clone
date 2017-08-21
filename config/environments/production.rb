7efbcd2aa4f7e2a9315d8ece7d439b62a3654feb
“Vilde <vilde@vevatne.no>
2017-08-21 09:33:42 -0700

    Configure right heroku url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # config.force_ssl = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'twitter-clone-in-rails.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
