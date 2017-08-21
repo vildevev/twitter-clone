fd734a4d0ba51d01b08b52f8ccb91aa6ef95c8a3
“Vilde <vilde@vevatne.no>
2017-08-21 13:19:44 -0700

    Add integration test for micropost interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,12 @@ class User < ApplicationRecord
 	  end 
 	end 
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed 
+    Micropost.where("user_id = ?", id)
+  end 
+
   # Remembers a user in the database for use in persistent sessions.
   def remember 
   	self.remember_token = User.new_token
